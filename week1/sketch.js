e5d1e4b11994bccf8dbfcbfa16656ad306f56081
DeadAugust <augustluhrs@gmail.com>
2019-03-28 14:02:59 -0400

    water bottle

--- week1/sketch.js
+++ week1/sketch.js
@@ -4,7 +4,7 @@ let classifier;
 function setup(){
   // noCanvas();
   video = createCapture(VIDEO);
-  createCanvas(width,height - video.height);
+  createCanvas(500,500);
   background(0,255,200);
 
   //set up image classifier with MobileNet and the video
@@ -12,7 +12,7 @@ function setup(){
 }
 
 function draw(){
-  
+
 }
 
 function modelReady(){
@@ -32,5 +32,14 @@ function gotResult(err, results){
   //array ordered by prob.
   select('#result').html(results[0].className);
   select('#probability').html(nf(results[0].probability, 0, 2));
+
+  if (results[0].className == 'water bottle'){
+    let randX = random(0, 500);
+    let randY = random(0, 500);
+    let randD = random(0, 500);
+    let randC = random(0, 255);
+    fill(0, 0, randC);
+    ellipse(randX, randY, randD, randD);
+  }
   classifyVideo();
 }
