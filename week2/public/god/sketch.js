3b0b74066bf52ab8a2c3693c75cd2760434aa3f3
DeadAugust <augustluhrs@gmail.com>
2019-04-10 16:00:29 -0400

    added hunger, weird bug where a bunch die at once

--- week2/public/god/sketch.js
+++ week2/public/god/sketch.js
@@ -11,6 +11,7 @@ KNN Classification on Webcam Images with mobileNet. Built with p5.js
 let video;
 let cnv;
 let start = false;
+let startOver;
 
 // Create a KNN classifier
 const knnClassifier = ml5.KNNClassifier();
@@ -22,13 +23,13 @@ const featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
 let socket = io('/god');
 
 // Listen for confirmation of connection
-socket.on('connect', function () {
+socket.on('connect', function() {
   console.log("Connected");
 });
 
 //godling variables
 let godSpeed = 5;
-let godHunger = 1000; //bigger b/c decreases by 1 each frame?
+let godHunger = 5000; //bigger b/c decreases by 1 each frame?
 let godHope = 1000;
 let godJoy = 1000;
 let godDespair = 10; //limit for killing
@@ -38,9 +39,27 @@ let godUpDown = [1, -1];
 let godLeftRight = [-1, 1];
 
 let godlings = [];
+let food = [];
 
-class Godling {//need high up b/c not hoisted?
-  constructor(x, y){ //spawns where the collision took place
+class Food {
+  constructor() {
+    this.x = random(10, width - 10);
+    this.y = random(10, height - 10);
+    this.size = random(10, 40);
+    this.supply = int(random(10, 500));
+  }
+  munch() {
+    this.supply--;
+  }
+  show() {
+    noStroke();
+    fill(0, 100, 255);
+    rect(this.x, this.y, this.size, this.size);
+  }
+}
+
+class Godling { //need high up b/c not hoisted?
+  constructor(x, y) { //spawns where the collision took place
     this.x = x;
     this.y = y;
     this.xSpeed = random(1, godSpeed);
@@ -55,35 +74,41 @@ class Godling {//need high up b/c not hoisted?
     this.joy = random(1, godJoy);
     this.dead = false; //weird splice issue
   }
-  move(){ //move the godlings in a random direction until they hit a wall
+  move() { //move the godlings in a random direction until they hit a wall
     this.hope = constrain(this.hope, 0, godHope);
     this.speed = map(this.hope, 1, godHope, 0, godSpeed);
-    if(this.x < 0 || this.x > width){
+    if (this.x < 0 || this.x > width) {
       this.left *= -1;
     }
-    if(this.y < 0 || this.y > height){
+    if (this.y < 0 || this.y > height) {
       this.up *= -1;
     }
     //this is gonna be weird, add? do something else? -- inital direction?
-    this.x += ((this.speed * this.xSpeed)/2) * this.left;
-    this.y += ((this.speed * this.ySpeed)/2) * this.up;
+    this.x += ((this.speed * this.xSpeed) / 2) * this.left;
+    this.y += ((this.speed * this.ySpeed) / 2) * this.up;
 
     //if food
 
 
   }
-  show(){ //update the display
+  show() { //update the display
     //slight decline
     this.joy--;
     this.hope--;
-    // this.hunger--;
-    //if fertile, highlight in white
-    if(this.dead){
+    this.hunger--;
+    //hungry, decreases in size
+    if (this.hunger <= 0){
+      this.dead = true;
+    }
+    this.size = map(this.hunger, 0, godHunger, 5, 50);
+    //if dead, kill it
+    if (this.dead) {
       godlings.splice(this, 1);
       return;
     }
-    if(this.fertility){
-      stroke(255,255,255);
+    //if fertile, highlight in white
+    if (this.fertility) {
+      stroke(255, 255, 255);
       strokeWeight(3);
     } else {
       noStroke();
@@ -92,22 +117,21 @@ class Godling {//need high up b/c not hoisted?
     let joyColor = map(this.joy, 1, godJoy, 0, 255);
     let sufferColor = 255 - joyColor;
     fill(sufferColor, joyColor, 20);
-    //hungry, decreases in size
-    this.size = map(this.hunger, 1, godHunger, 5, 50);
+
     //show
     ellipse(this.x, this.y, this.size, this.size);
   }
-  eat(){
+  eat() {
 
   }
-  spawn(){ //if two fertile godlings hit, they pop out a new one
+  spawn() { //if two fertile godlings hit, they pop out a new one
 
   }
-  kill(){ //if a suffering godling hits another godling, that godling dies
+  kill() { //if a suffering godling hits another godling, that godling dies
 
   }
 
-  intersects(other){
+  intersects(other) {
     //return true if they're touching
     return ((dist(this.x, this.y, other.x, other.y)) < (this.size + other.size));
     // let sharedSpace = this.size + other.size; //too much computing?
@@ -116,8 +140,8 @@ class Godling {//need high up b/c not hoisted?
     // }
   }
 
-  loveAndHate(other){
-    if(this.fertility && other.fertility){ //if both fertile, make a baby
+  loveAndHate(other) {
+    if (this.fertility && other.fertility) { //if both fertile, make a baby
       let baby = new Godling(this.x, other.y);
       godlings.push(baby); //pushhhh!!!
       this.fertility = false; //turn off or will make two?
@@ -125,63 +149,15 @@ class Godling {//need high up b/c not hoisted?
     }
     // let thisDead = false;
     // let jDead = false;
-    if(this.joy < godDespair){ //if i is despairing
+    if (this.joy < godDespair) { //if i is despairing
       console.log("them dead" + this.joy + " X " + other.joy);
       other.dead = true;
     }
-    if(other.joy < godDespair){ //if j is despairing
+    if (other.joy < godDespair) { //if j is despairing
       console.log("this dead" + this.joy + " X " + other.joy);
       other.dead = true;
     }
   }
-/*
-  collide(){
-    // if lover or enemy
-    // console.log(this);
-    for (let j = godlings.length -1; j >= 0; j--){
-      let them = godlings[j];
-      if (them !== this){
-        let sharedSpace = this.size + them.size; //too much computing?
-        if (dist(this.x, this.y, them.x, them.y) < sharedSpace){
-          // console.log('hit' + this + them);
-          // console.log(this);
-          if(this.fertility && them.fertility){ //if both fertile, make a baby
-            let baby = new Godling(this.x, them.y);
-            godlings.push(baby); //pushhhh!!!
-            this.fertility = false; //turn off or will make two?
-            them.fertility = false;
-          }
-          // let thisDead = false;
-          // let jDead = false;
-          if(this.joy < godDespair){ //if i is despairing
-            console.log("them dead" + this.joy + " X " + them.joy);
-            them.dead = true;
-          }
-          if(them.joy < godDespair){ //if j is despairing
-            console.log("this dead" + this.joy + " X " + them.joy);
-            this.dead = true;
-          }
-          //if either was killed
-
-          // if(thisDead){
-          //   godlings.splice(i, 1);
-          //   if(jDead){ //have to mess with index because of splice...
-          //     if(i < j){
-          //       godlings.splice(j-1, 1);
-          //     } else{
-          //       godlings.splice(j, 1);
-          //     }
-          //   }
-          // } else if (jDead){
-          //   godlings.splice(j, 1);
-          // }
-
-        }
-      }
-    }
-  }
-*/
-
 }
 
 function setup() {
@@ -194,6 +170,10 @@ function setup() {
   video.parent('videoContainer');
   // Create the UI buttons
   createButtons();
+  startOver = createButton('start world over')
+    .mousePressed(function(){
+      bloom();
+    });
 
   //socket events
 
@@ -203,47 +183,65 @@ function setup() {
   //   let data = message.data;
   // }
 
-  //starting godlings
-  for (let i = 0; i < 10; i ++){
-    let randStart = random(-200, 200);
-    let godling = new Godling(width/2 + randStart, height/2 + randStart);
-    godlings.push(godling);
-  }
+  //starting stuff
+  bloom()
 }
 
-function draw(){
-  if(start){
+function draw() {
+  if (start) {
     background(0);
-    for (let i = godlings.length - 1; i >= 0; i--){
+    for (let f = food.length - 1; f >= 0; f--) {
+      if (food.supply <= 0) {
+        food.splice(f, 1);
+      } else {
+        food[f].show();
+      }
+    }
+    for (let i = godlings.length - 1; i >= 0; i--) {
       godlings[i].move();
       // godlings[i].collide(i);
       godlings[i].show();
     }
-    for (let i = godlings.length - 1; i >= 0; i--){
-      for (let j = i -1; j >= 0; j--){
-        if (godlings[i].intersects(godlings[j])){
+    for (let i = godlings.length - 1; i >= 0; i--) {
+      for (let j = i - 1; j >= 0; j--) {
+        if (godlings[i].intersects(godlings[j])) {
           godlings[i].loveAndHate(godlings[j]);
         }
       }
+      for (let f of food){
+        if (godlings[i].intersects(f)){
+          f.supply--;
+          if(godlings[i].hunger < godHunger){
+            godlings[i].hunger += 50;
+          }
+        }
+      }
       //check for death
-      if (godlings[i].dead){
+      if (godlings[i].dead) {
         godlings.splice(i, 1);
       }
     }
   }
 }
 
-/*
--------------- p5 stuff
-*/
-
+function bloom(){
+  //starting godlings
+  for (let i = 0; i < 10; i++) {
+    let randStart = random(-200, 200);
+    let godling = new Godling(width / 2 + randStart, height / 2 + randStart);
+    godlings.push(godling);
+    //starting Food
+    let startFood = new Food();
+    food.push(startFood);
+  }
+}
 
 
 /*
 -------------- KNN Functions
 */
 
-function modelReady(){
+function modelReady() {
   select('#status').html('FeatureExtractor(mobileNet model) Loaded')
 }
 
@@ -269,7 +267,7 @@ function classify() {
     console.error('There is no examples in any class');
     return;
   }
-  if (!start){
+  if (!start) {
     start = true;
   }
   // Get the features of the input video
@@ -294,53 +292,53 @@ function createButtons() {
   buttonSmite = select('#addSmite')
     .mousePressed(function() {
       addExample('Smite');
-  });
+    });
 
   buttonFertilize = select('#addFertilize')
     .mousePressed(function() {
       addExample('Fertilize');
-  });
+    });
 
   buttonFood = select('#addFood')
     .mousePressed(function() {
       addExample('Food');
-  });
+    });
 
   buttonHope = select('#addHope')
     .mousePressed(function() {
       addExample('Hope');
-  });
+    });
 
   buttonJoy = select('#addJoy')
     .mousePressed(function() {
       addExample('Joy');
-  });
+    });
 
   // Reset buttons
   resetSmite = select('#resetSmite')
     .mousePressed(function() {
       clearClass('Smite');
-  });
+    });
 
   resetFertilize = select('#resetFertilize')
     .mousePressed(function() {
       clearClass('Fertilize');
-  });
+    });
 
   resetFood = select('#resetFood')
     .mousePressed(function() {
       clearClass('Food');
-  });
+    });
 
   resetHope = select('#resetHope')
     .mousePressed(function() {
       clearClass('Hope');
-  });
+    });
 
   resetJoy = select('#resetJoy')
     .mousePressed(function() {
       clearClass('Joy');
-  });
+    });
 
   // Predict button
   buttonPredict = select('#buttonPredict');
@@ -374,24 +372,25 @@ function gotResults(err, result) { //prob where the action happens
       select('#confidence').html(`${confideces[result.label] * 100} %`);
     }
 
-    if (result.label == 'Smite'){
+    if (result.label == 'Smite') {
 
     }
-    if (result.label == 'Fertilize'){
+    if (result.label == 'Fertilize') {
       let babyMaker = random(godlings);
       babyMaker.fertility = true;
     }
-    if (result.label == 'Food'){
-
+    if (result.label == 'Food') {
+      let newFood = new Food();
+      food.push(newFood);
     }
-    if (result.label == 'Hope'){
-      for (let g of godlings){
+    if (result.label == 'Hope') {
+      for (let g of godlings) {
         g.hope += 50;
         g.hope = constrain(g.hope, 0, godHope);
       }
     }
-    if (result.label == 'Joy'){
-      for (let g of godlings){
+    if (result.label == 'Joy') {
+      for (let g of godlings) {
         g.joy += 50;
         g.joy = constrain(g.joy, 0, godJoy);
 
